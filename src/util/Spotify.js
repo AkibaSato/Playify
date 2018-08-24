5120b2e1308164e5fca0410e3d84e9213a209494
Akiba Sato <akibasato1992@gmail.com>
2018-08-23 22:12:22 -0400

    Added console logs for testing.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -33,26 +33,31 @@ const Spotify = {
   },
 
   search(term) {
-    const searchUrl = `https://api.spotify.com/v1/search?type=track&q=${term.replace(' ', '%20')}`;
-    return fetch(searchUrl, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`
-        }
-      })
-      .then(response => response.json())
-      .then(jsonResponse => {
-        if (!jsonResponse.tracks) return [];
-        return jsonResponse.tracks.items.map(track => {
-          console.log(track);
-          return {
-            id: track.id,
-            name: track.name,
-            artist: track.artists[0].name,
-            album: track.album.name,
-            uri: track.uri
+    try {
+      Spotify.getAccessToken();
+      const searchUrl = `https://api.spotify.com/v1/search?type=track&q=${term}`;
+      return fetch(searchUrl, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`
           }
         })
-      });
+        .then(response => response.json())
+        .then(jsonResponse => {
+          if (!jsonResponse.tracks) return [];
+          return jsonResponse.tracks.items.map(track => {
+            console.log(track);
+            return {
+              id: track.id,
+              name: track.name,
+              artist: track.artists[0].name,
+              album: track.album.name,
+              uri: track.uri
+            }
+          })
+        });
+    } catch(error) {
+      console.log(error);
+    }
   },
 
   savePlaylist(name, trackUris) {
